1fb5aa7913b095f68676f27c55a623b8c2814e8d
Aaron Liberatore <aaron.liberatore@gmail.com>
2016-03-12 12:42:59 +0100

    Corrects movement to traditional style

--- utilitiestransform.js
+++ utilitiestransform.js
@@ -8,14 +8,14 @@ function rotate(theta,pts,dir){
 
     for(var i = 0;i < pts.length; i++){
         pt = pts[i];
-        t_pts.push([ship.x + cos * (pt[0] - ship.x) - sin * (pt[1] - ship.y),
-                    ship.y + sin * (pt[0] - ship.x) + cos * (pt[1] - ship.y)]);
+        t_pts.push([cos * pt[0] - sin * pt[1],
+                    sin * pt[0] + cos * pt[1]]);
     }
     dir = [cos * dir[0] - sin *  dir[1], sin *  dir[0] + cos *  dir[1]];
     return {t_pts:t_pts,dir,dir};
 }
 /*Generic Sprite creation
-Inputs: x,y starting coordinates and a list of transformations for sprites points
+Inputs: x,y starting coordinates and a list of getVeticesPosations for sprites points
 The first [x,y] transformation should be the front of the obj. i.e.
 The point that will shoot
 
@@ -24,17 +24,22 @@ function Sprite(x,y,coor){
     this.x = x
     this.y = y
     this.scale = 1,
-    this.dim = coor,
-    this.coor = []
-    for(var i=0;i<coor.length;i++){
-        this.coor.push([x+coor[i][0] * this.scale,y+coor[i][1]* this.scale]);
-    }
+    this.coor = coor,
     this.dir  = [0,1]
     this.vel  = 0
     this.theta = 0,
 
     this.shape = "./images/shapeship.png"
+
+    this.getVerticesPos = function(){
+        var pts = [];
+        for(var i = 0; i < this.coor.length;i++){
+            pts.push([this.x + (this.coor[i][0] * this.scale), this.y + (this.coor[i][1] * this.scale)]);
+        }
+        return pts;
+    }
     this.rotate = function(theta){
+        var tmp = this.getVerticesPos();
         var r = rotate(theta,this.coor,this.dir);
         this.coor = r.t_pts;
         this.dir  = r.dir;
@@ -51,30 +56,7 @@ function Sprite(x,y,coor){
             }
         }
     }
-    this.createCoor = function(){
-        var tmp = [];
-        for(var i=0;i<this.dim.length;i++){
-            tmp.push([this.x + this.dim[i][0] * this.scale,this.y + this.dim[i][1]* this.scale]);
-        }
-        this.coor = tmp;
-    }
-    this.boundsCheck = function(){
-        if (this.x >= canvas.width) {
-            this.x = this.x % canvas.width;
-            this.createCoor();
-        } else if (this.x <= 0) {
-            this.x = this.x + canvas.width;
-            this.createCoor();
-        }
-        if (this.y >= canvas.height) {
-            this.y = this.y % canvas.height;
-            this.createCoor();
-        } else if (this.y <= 0) {
-            this.y = this.y + canvas.height;
-            this.createCoor();
-        }
 
-    }
 }
 function SafeSprite(){
     this.x = 250
@@ -93,7 +75,7 @@ function SafeSprite(){
         this.right = pts.t_pts[2];
         this.dir   = pts.dir;
     }
-    this.update = function(){
+    this.update = function(theta){
         if(this.vel != 0){
             var dirVX = this.vel * this.dir[0];
             var dirVY = this.vel * this.dir[1];
@@ -198,10 +180,11 @@ function repaintShip(){
 
     ship.rotate(ship.theta);
     ctx.beginPath();
-    var coor = ship.coor;
-    ctx.moveTo(coor[0][0],coor[0][1]);
-    for(var i = 1; i < coor.length;i++){
-        ctx.lineTo(coor[i][0],coor[i][1]);
+    var verts = ship.getVerticesPos();
+    //console.log(verts);
+    ctx.moveTo(verts[0][0],verts[0][1]);
+    for(var i = 1; i < verts.length;i++){
+        ctx.lineTo(verts[i][0],verts[i][1]);
     }
     ctx.closePath();
     ctx.lineWidth = 2;
@@ -210,7 +193,7 @@ function repaintShip(){
 }
 // Purpose: Redraw Shots
 function repaintShots(){
-  for (var l=0;l<shots.length;l++){
+  for (l=0;l<shots.length;l++){
     // adjust coordinates
     shots[l][1] += shots[l][3];
     shots[l][0] += shots[l][2];
@@ -247,32 +230,58 @@ function repaintShots(){
   }
 }
 function repaintAliens(){
-    console.log(aliens.length);
-    for (var t=0;t<aliens.length;t++){
-      // adjust coordinates
-
-      var sprite = aliens[t];
-      sprite.update();
-      sprite.boundsCheck();
-      ctx.beginPath();
-      var coor = sprite.coor;
-      ctx.moveTo(coor[0][0],coor[0][1]);
-      for(var i = 1; i < coor.length;i++){
-          ctx.lineTo(coor[i][0] * sprite.scale,coor[i][1] * sprite.scale);
-      }
-      ctx.closePath();
-      ctx.lineWidth = 1;
-      ctx.strokeStyle = 'Red';
-      ctx.stroke();
+  for (t=0;t<aliens.length;t++){
+    // adjust coordinates
+    aliens[t][1] += aliens[t][3];
+    aliens[t][0] += aliens[t][2];
+    var Cx = aliens[t][0];
+    var Cy = aliens[t][1];
+    if(Cx > canvas.width ){
+      aliens[t][0] = Cx % canvas.width;
+      aliens[t][1] = Cy;
+    } else if ( Cx < 0 ){
+      aliens[t][0] = Cx + canvas.width;
+      aliens[t][1] = Cy;
     }
+    if(Cy > canvas.height){
+      aliens[t][0] = Cx;
+      aliens[t][1] = Cy % canvas.height;
+    } else if (Cy < 0) {
+      aliens[t][0] = Cx;
+      aliens[t][1]= Cy + canvas.height;
+    }
+    ctx.beginPath();
+    ctx.moveTo(aliens[t][0]-10, aliens[t][1]);
+    ctx.lineTo(aliens[t][0]-5, aliens[t][1]+5);
+    ctx.lineTo(aliens[t][0]+5, aliens[t][1]+5);
+    ctx.lineTo(aliens[t][0]+10, aliens[t][1]);
+    ctx.lineTo(aliens[t][0]+5, aliens[t][1]-5);
+
+    ctx.lineTo(aliens[t][0]-5, aliens[t][1]-5);
+    ctx.lineTo(aliens[t][0]+5, aliens[t][1]-5);
+
+    ctx.lineTo(aliens[t][0]+4, aliens[t][1]-5);
+    ctx.lineTo(aliens[t][0]+2, aliens[t][1]-8);
+    ctx.lineTo(aliens[t][0]-2, aliens[t][1]-8);
+    ctx.lineTo(aliens[t][0]-4, aliens[t][1]-5);
+    ctx.lineTo(aliens[t][0]-5, aliens[t][1]-5);
+    ctx.lineTo(aliens[t][0]-10, aliens[t][1]);
+    ctx.lineTo(aliens[t][0]+10, aliens[t][1]);
+    ctx.closePath();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'Red';
+    ctx.stroke();
+  }
 }
 // Purpose: Redraw Asteroids
 function repaintAsteroids(){
-  for (var k=0;k<asteroids.length;k++){
+  for (i=0;i<asteroids.length;i++){
     // adjust coordinates
-    var sprite = asteroids[k];
+    var sprite = asteroids[i];
+    sprite.x += sprite.dir[0];
+    sprite.y += sprite.dir[1];
     sprite.update();
-    sprite.boundsCheck();
+    //sprite.boundsCheck();
 
     ctx.beginPath();
     var coor = sprite.coor;
