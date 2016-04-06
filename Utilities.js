826eb679668506da514ae68602470910c620c3f0
Aaron Liberatore <aaron.liberatore@gmail.com>
2016-04-06 13:11:31 -0400

    Builds around sprites

--- Utilities.js
+++ Utilities.js
@@ -14,120 +14,92 @@ function rotate(theta,pts,dir){
     dir = [cos * dir[0] - sin *  dir[1], sin *  dir[0] + cos *  dir[1]];
     return {t_pts:t_pts,dir,dir};
 }
+function boundsCheck(x,y){
+    if (x >= canvas.width) {
+        x = x % canvas.width;
+    } else if (x <= 0) {
+        x = x + canvas.width;
+    }
+    if (y >= canvas.height) {
+        y = y % canvas.height;
+    } else if (y <= 0) {
+        y = y + canvas.height;
+    }
+    return {x:x,y:y}
+}
+//Purpose: Creates a new randomly located object with a random velocity
+function object(transforms, size) {
+    var coors = getRandomCoordinates();
+    var velocity = getRandomVelocity();
+
+    if (ship.x+50 >= coors[0] && ship.x-50 <= coors[0] && ship.y+50 >= coors[1] && ship.y-50 <= coors[1]){
+      coors[0]+=100;
+      coors[1]+=100;
+    }
+
+    var object = new Sprite(coors[0],coors[1],transforms);
+
+    object.vDir = velocity;
+    object.vel = 1;
+    object.scale = 1;
+
+    return object
+}
 /*Generic Sprite creation
 Inputs: x,y starting coordinates and a list of transformations for sprites points
 The first [x,y] transformation should be the front of the obj. i.e.
 The point that will shoot
 
 */
-function Sprite(x,y,coor){
-    this.x = x
-    this.y = y
+function Sprite(x,y,transform){
+    this.x = x,
+    this.y = y,
     this.scale = 1,
-    this.dim = coor,
-    this.coor = []
-    for(var i=0;i<coor.length;i++){
-        this.coor.push([x+coor[i][0] * this.scale,y+coor[i][1]* this.scale]);
-    }
-    this.dir  = [0,1]
-    this.vel  = 0
+    this.transform = transform,
+    this.dir   = [0,1], // gun direction
+    this.vDir  = [0,1],
+    this.vel   = 0,
+    this.rVel  = 0,
     this.theta = 0,
 
     this.shape = "./images/shapeship.png"
-    this.rotate = function(theta){
-        var r = rotate(theta,this.coor,this.dir);
-        this.coor = r.t_pts;
-        this.dir  = r.dir;
+    this.rotate = function(theta,pts){
+        return rotate(theta,pts,this.dir);
     }
-    this.update = function(){
-        if(this.vel != 0){
-            var dirVX = this.vel * this.dir[0];
-            var dirVY = this.vel * this.dir[1];
+    this.update = function(){ // Specific to the asteroids movement style
+        // if the velocity vector is nonnegligible continue moving in the vDirection
+        // otherwise change directions to the tip direction
+        if(this.vel > 0.25){
+            var dirVX = this.vel * this.vDir[0];
+            var dirVY = this.vel * this.vDir[1];
             this.x = this.x + dirVX;
             this.y = this.y + dirVY;
-            var tmp = [];
-            for(var i = 0; i < this.coor.length;i++){
-                this.coor[i] = [this.coor[i][0] + dirVX, this.coor[i][1] + dirVY];
-            }
+        } else{
+            this.vDir = this.dir;
+        }
+        if(this.rVel != 0){
+            var r = rotate(ship.rVel,[[0,0]],this.dir);
+            this.dir  = r.dir;
         }
     }
-    this.createCoor = function(){
+    this.genVertices = function(){
+        var tra = [];
+        for(var i=0;i<this.transform.length;i++){
+            tra.push([this.x+this.transform[i][0] * this.scale,this.y+this.transform[i][1]* this.scale]);
+        }
+
+        return tra;
+    }
+    this.getVerticesPos = function(){
         var tmp = [];
-        for(var i=0;i<this.dim.length;i++){
-            tmp.push([this.x + this.dim[i][0] * this.scale,this.y + this.dim[i][1]* this.scale]);
-        }
-        this.coor = tmp;
+        var pts = this.genVertices();
+        var rPt = this.rotate(this.theta,pts);
+        return rPt.t_pts
     }
     this.boundsCheck = function(){
-        if (this.x >= canvas.width) {
-            this.x = this.x % canvas.width;
-            this.createCoor();
-        } else if (this.x <= 0) {
-            this.x = this.x + canvas.width;
-            this.createCoor();
-        }
-        if (this.y >= canvas.height) {
-            this.y = this.y % canvas.height;
-            this.createCoor();
-        } else if (this.y <= 0) {
-            this.y = this.y + canvas.height;
-            this.createCoor();
-        }
-
-    }
-}
-function SafeSprite(){
-    this.x = 250
-    this.y = 250
-    this.front = [250,256]
-    this.left  = [246,246]
-    this.right = [254,246]
-    this.dir   = [0,1]
-    this.vel  = 0
-    this.theta = 0,
-    this.shape = "./images/shapeship.png"
-    this.rotate = function(theta){
-        pts = rotate(theta,[this.front,this.left,this.right],this.dir);
-        this.front = pts.t_pts[0];
-        this.left  = pts.t_pts[1];
-        this.right = pts.t_pts[2];
-        this.dir   = pts.dir;
-    }
-    this.update = function(){
-        if(this.vel != 0){
-            var dirVX = this.vel * this.dir[0];
-            var dirVY = this.vel * this.dir[1];
-            this.x = this.x + dirVX;
-            this.y = this.y + dirVY;
-            this.front = [this.front[0] + dirVX,this.front[1] + dirVY];
-            this.left  = [this.left[0]  + dirVX,this.left[1]  + dirVY];
-            this.right = [this.right[0] + dirVX,this.right[1] + dirVY];
-        }
-    }
-    this.boundsCheck = function(){
-        if (this.x >= canvas.width) {
-            this.x = this.x % canvas.width;
-            this.front = [this.front[0] % canvas.width,this.front[1]];
-            this.left  = [this.left[0]  % canvas.width,this.left[1] ];
-            this.right = [this.right[0] % canvas.width,this.right[1]];
-        } else if (this.x <= 0) {
-            this.x = this.x + canvas.width;
-            this.front = [this.front[0] + canvas.width,this.front[1]];
-            this.left  = [this.left[0]  + canvas.width,this.left[1] ];
-            this.right = [this.right[0] + canvas.width,this.right[1]];
-        }
-        if (this.y >= canvas.height) {
-            this.y = this.y % canvas.height;
-            this.front = [this.front[0],this.front[1] % canvas.height];
-            this.left  = [this.left[0] ,this.left[1]  % canvas.height ];
-            this.right = [this.right[0],this.right[1] % canvas.height];
-        } else if (this.y <= 0) {
-            this.y = this.y + canvas.height;
-            this.front = [this.front[0],this.front[1] + canvas.height];
-            this.left  = [this.left[0] ,this.left[1]  + canvas.height ];
-            this.right = [this.right[0],this.right[1] + canvas.height];
-        }
-
+        coor = boundsCheck(this.x,this.y);
+        this.x = coor.x;
+        this.y = coor.y;
     }
 }
 //////////////////////////////////////////////
@@ -137,31 +109,38 @@ function SafeSprite(){
 //Purpose: Computes whether a point is in a polgon using the # of edges intercepted by a horizonal line through the pt
 //Inputs: points: coordinates of the polygon corners, pt: the testing point
 function point_in_polygon(points,pt){
-  var m = 0;
-  var r = points.length-1;
-  var oddNodes = false;
-  for (m=0; m<points.length; m++) {
-    if ((points[m][1]< pt[1] && points[r][1]>=pt[1]
-    ||   points[r][1]< pt[1] && points[m][1]>=pt[1])
-    &&  (points[m][0]<=pt[0] || points[r][0]<=pt[0])) {
-      if (points[m][0]+(pt[1]-points[m][1])/(points[r][1]-points[m][1])*(points[r][0]-points[m][0])<pt[0]) {
-        if(oddNodes == false){  oddNodes = true; } else{oddNodes = false;}
-      }
+    var m = 0;
+    var r = points.length-1;
+    var oddNodes = false;
+    for (m=0; m<points.length; m++) {
+
+        if ((points[m][1]< pt[1] && points[r][1]>=pt[1]
+        ||   points[r][1]< pt[1] && points[m][1]>=pt[1])
+        &&  (points[m][0]<=pt[0] || points[r][0]<=pt[0])) {
+
+            if (points[m][0]+(pt[1]-points[m][1])/(points[r][1]-points[m][1])*(points[r][0]-points[m][0])<pt[0]) {
+                if(oddNodes == false){
+                    oddNodes = true;
+                }
+                else{
+                    oddNodes = false;
+                }
+            }
+        }
+
+        r = m;
     }
-    r=m;
-  }
-  return oddNodes;
+    return oddNodes;
 }
 
 //Purpose: Consolidates checking multiple points (space ship)
 function pts_in_polygon(points,pts){
-    ret = false;
     for (a=0; a<pts.length; a++) {
-      if(point_in_polygon(points,pts[a])){
-        ret = true;
-      };
+        if(point_in_polygon(points,pts[a])){
+            return true;
+        };
     }
-    return ret;
+    return false;
 }
 
 //////////////////////////////////////////////
@@ -170,14 +149,14 @@ function pts_in_polygon(points,pts){
 
 // Purpose: Output Random Coordinates based on the canvas
 function getRandomCoordinates() {
-  return [Math.floor(Math.random() * (canvas.height - 1)),Math.floor(Math.random() * (canvas.height - 1))];
+    return [Math.floor(Math.random() * (canvas.height - 1)),Math.floor(Math.random() * (canvas.height - 1))];
 }
 
 // Purpose: Output Random (x,y) velocities
 function getRandomVelocity() {
-  var plusOrMinusX = Math.random() < 0.5 ? -1 : 1;
-  var plusOrMinusY = Math.random() < 0.5 ? -1 : 1;
-  return [plusOrMinusX*Math.random(),plusOrMinusY*Math.random()];
+    var plusOrMinusX = Math.random() < 0.5 ? -1 : 1;
+    var plusOrMinusY = Math.random() < 0.5 ? -1 : 1;
+    return [plusOrMinusX*Math.random(),plusOrMinusY*Math.random()];
 }
 
 //////////////////////////////////////////////
@@ -186,103 +165,66 @@ function getRandomVelocity() {
 
 //Repaint functions: redraw the ship, shots, and asteroids at their new locations
 function repaint() {
-  repaintShip();
+  repaintSprites();
   repaintShots();
-  repaintAliens();
-  repaintAsteroids();
-}
-// Purpose: Redraw Ship
-function repaintShip(){
-    checkShipCollision();
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    ship.rotate(ship.theta);
-    ctx.beginPath();
-    var coor = ship.coor;
-    ctx.moveTo(coor[0][0],coor[0][1]);
-    for(var i = 1; i < coor.length;i++){
-        ctx.lineTo(coor[i][0],coor[i][1]);
-    }
-    ctx.closePath();
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = 'White';
-    ctx.stroke();
 }
 // Purpose: Redraw Shots
 function repaintShots(){
-  for (var l=0;l<shots.length;l++){
-    // adjust coordinates
-    shots[l][1] += shots[l][3];
-    shots[l][0] += shots[l][2];
-    shots[l][4] += 1;
-    if(shots[l][4] > 500){
-      shots.splice(l,1);
-    }
-    else {
-      //wrap around page
-      var Cx = shots[l][0];
-      var Cy = shots[l][1];
-      if(Cx > canvas.width ){
-        shots[l][0] = Cx % canvas.width;
-        shots[l][1] = Cy;
-      } else if ( Cx < 0 ){
-        shots[l][0] = Cx + canvas.width;
-        shots[l][1] = Cy;
-      }
-      if(Cy > canvas.height){
-        shots[l][0] = Cx;
-        shots[l][1] = Cy % canvas.height;
-      } else if (Cy < 0) {
-        shots[l][0] = Cx;
-        shots[l][1] = Cy + canvas.height;
-      }
-      ctx.beginPath();
-      ctx.arc(shots[l][0],shots[l][1], 2, 0, Math.PI * 2);
-      ctx.fillStyle = shots[l][5];
-      ctx.fill();
-      if(!checkAsteroidCollision(shots[l],l)){
-        checkAlienShipCollision(shots[l],l);
-      }
-    }
-  }
-}
-function repaintAliens(){
-    console.log(aliens.length);
-    for (var t=0;t<aliens.length;t++){
-      // adjust coordinates
+    for (var l=0;l<shots.length;l++){
+        // adjust coordinates
 
-      var sprite = aliens[t];
-      sprite.update();
-      sprite.boundsCheck();
-      ctx.beginPath();
-      var coor = sprite.coor;
-      ctx.moveTo(coor[0][0],coor[0][1]);
-      for(var i = 1; i < coor.length;i++){
-          ctx.lineTo(coor[i][0] * sprite.scale,coor[i][1] * sprite.scale);
-      }
-      ctx.closePath();
-      ctx.lineWidth = 1;
-      ctx.strokeStyle = 'Red';
-      ctx.stroke();
+        shots[l][1] += shots[l][3];
+        shots[l][0] += shots[l][2];
+        shots[l][4] += 1;
+        if(shots[l][4] > 500){
+            shots.splice(l,1);
+        }
+        else {
+            //wrap around page
+            coor = boundsCheck(shots[l][0],shots[l][1]);
+            shots[l][0] = coor.x;
+            shots[l][1] = coor.y;
+
+            ctx.beginPath();
+            ctx.arc(shots[l][0],shots[l][1], 2, 0, Math.PI * 2);
+            ctx.fillStyle = shots[l][5];
+            ctx.fill();
+
+            //Check bullet collisions
+            if(!checkAsteroidCollision(shots[l],l)){
+                checkAlienShipCollision(shots[l],l);
+            }
+            checkShipCollisionBullets();
+        }
     }
 }
-// Purpose: Redraw Asteroids
-function repaintAsteroids(){
-  for (var k=0;k<asteroids.length;k++){
-    // adjust coordinates
-    var sprite = asteroids[k];
-    sprite.update();
+function paint(sprite,color){
     sprite.boundsCheck();
 
+    var coor = sprite.getVerticesPos();
+
     ctx.beginPath();
-    var coor = sprite.coor;
     ctx.moveTo(coor[0][0],coor[0][1]);
     for(var i = 1; i < coor.length;i++){
         ctx.lineTo(coor[i][0] * sprite.scale,coor[i][1] * sprite.scale);
     }
     ctx.closePath();
     ctx.lineWidth = 1;
-    ctx.strokeStyle = 'White';
+    ctx.strokeStyle = color;
     ctx.stroke();
-  }
+}
+function repaintSprites(){
+    checkShipCollision();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ship.theta += ship.rVel;
+    paint(ship,"yellow");
+
+    for (var t=0;t<asteroids.length;t++){
+        asteroids[t].update();
+        paint(asteroids[t],'white');
+    }
+    for (var t=0;t<aliens.length;t++){
+        aliens[t].update();
+        paint(aliens[t],'red');
+    }
 }
